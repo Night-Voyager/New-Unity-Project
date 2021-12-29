49ac5a3af8eee1a645e31017ed60f7e6b850c0c8
Night-Voyager <18722007@bjtu.edu.cn>
2021-12-29 11:33:39 +0800

    feat: implement Bo on Player 1

---
+++ Assets/Scripts/Player_1_Controller.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_1_Controller : MonoBehaviour
+{
+    private static Animator animator;
+
+    enum State {Chu, Bo, Fang, Out};
+
+    public int state;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void getBo() {
+        Debug.Log("Player 1 got a Bo.");
+
+        if (state != (int)State.Fang) {
+            state = (int)State.Out;
+            animator.SetTrigger("Out");
+            Debug.Log("Player 1 is out.");
+        }
+    }
+}
