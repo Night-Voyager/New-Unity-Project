5e964bcd6cc4d6f0e7348adb058e48b64f749c2d
Night-Voyager <18722007@bjtu.edu.cn>
2021-12-29 20:15:34 +0800

    feat: blink when Player 0 is out

--- Assets/Scripts/Player_0_Controller.cs
+++ Assets/Scripts/Player_0_Controller.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class Player_0_Controller : MonoBehaviour
 {
+    private Renderer playerRenderer;
+
     enum State {Chu, Bo, Fang, Out};
 
     public int state;
     public int qi;
 
+    public int blinkNumber;
+    public float blinkTime;
+
     // Start is called before the first frame update
     void Start()
     {
+        playerRenderer = GetComponent<Renderer>();
         state = (int)State.Chu;
         qi = 0;
     }
@@ -41,6 +47,7 @@ public class Player_0_Controller : MonoBehaviour
         {
             state = (int)State.Out;
             Debug.Log("You are out.");
+            Blink();
         }
     }
 
@@ -48,5 +55,20 @@ public class Player_0_Controller : MonoBehaviour
     {
         Debug.Log("防");
         state = (int)State.Fang;
+    }
+
+    void Blink()
+    {
+        StartCoroutine(DoSomeBlinks(blinkNumber, blinkTime));
+    }
+
+    IEnumerator DoSomeBlinks(int number, float time)
+    {
+        for (int i=0; i<number; i++)
+        {
+            GetComponent<Renderer>().enabled = !GetComponent<Renderer>().enabled;
+            yield return new WaitForSeconds(time);
+        }
+        GetComponent<Renderer>().enabled = true;
     }
 }
